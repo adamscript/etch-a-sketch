dcec18571d43b459173b6b51ba9733dbcb4e1b13
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-05-15 09:27:13 +0800

    Made the grid rows and columns dynamic

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-const container = document.querySelector('#container');
-
 function createGrid(rows, columns){
+    container.style.gridTemplateColumns = ("auto ").repeat(columns)
+    container.style.gridTemplateRows = ("auto ").repeat(rows)
+
     for(let i = 1; i <= rows * columns; i++){
         const square = document.createElement('div');
         square.classList.add('square_div');
