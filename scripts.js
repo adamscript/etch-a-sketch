18d4ac239a3f2baa22563bba54500250ad147a5c
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-05-15 11:54:22 +0800

    Added fixed container size functionality

--- scripts.js
+++ scripts.js
@@ -1,31 +1,30 @@
 function newGrid(){
-    rowsInput = prompt("Enter number of rows : (Max 100)")
-    columnsInput = prompt("Enter number of columns : (Max 100)")
+    pixelInput = prompt("Enter pixel density : (Max 100)")
     
-    if(rowsInput > 100 || columnsInput > 100){
-        alert("Number too high :(");
+    if(pixelInput > 100){
+        alert("Size too high :(");
         return
     }
     else{
         clearGrid();
-        createGrid(rowsInput, columnsInput);
+        createGrid(pixelInput);
     }
 }
 
-function createGrid(rows, columns){
+function createGrid(pixels){
     const container = document.getElementById('container')
 
     while(container.firstChild){
         container.removeChild(container.firstChild);
     }
 
-    container.style.gridTemplateRows = ("auto ").repeat(rows);
-    container.style.gridTemplateColumns = ("auto ").repeat(columns);
+    container.style.gridTemplateRows = ("1fr ").repeat(pixels);
+    container.style.gridTemplateColumns = ("1fr ").repeat(pixels);
 
-    for(let i = 1; i <= rows * columns; i++){
+    for(let i = 1; i <= pixels * pixels; i++){
         const square = document.createElement('div');
         square.classList.add('square_div');
-        square.addEventListener("mouseover", () => {square.style.backgroundColor = "gold"})
+        square.addEventListener("mouseover", () => {square.style.backgroundColor = "grey"})
         container.appendChild(square);
     }
     return
@@ -34,9 +33,9 @@ function createGrid(rows, columns){
 function clearGrid(){
     let elements = document.getElementsByClassName('square_div');
     for(let i = 0; i < elements.length; i++){
-        elements[i].style.backgroundColor = "brown";
+        elements[i].style.backgroundColor = "white";
     }
     return
 }
 
-createGrid(16, 16);
+createGrid(16);
