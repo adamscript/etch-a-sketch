7432807ab9440592e3a9b45145eb0b74ad7d6e21
Aji Aditya Darmawan <ajiaditya27@gmail.com>
2021-05-15 09:01:27 +0800

    Created 16x16 grid

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-prompt("Hello worlddd")
+const container = document.querySelector('#container');
+
+function createGrid(rows, columns){
+    for(let i = 1; i <= rows * columns; i++){
+        const square = document.createElement('div');
+        square.classList.add('square_div');
+        container.appendChild(square);
+    }
+}
+
+createGrid(16, 16)
